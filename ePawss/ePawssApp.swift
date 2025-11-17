ae0d679aeb7fd8a578c62241c21b7955c98f2430
kathya <Q>
2025-11-17 11:00:32 -0600

    Initial Commit

---
+++ ePawss/ePawssApp.swift
@@ -0,0 +1,17 @@
+//
+//  ePawssApp.swift
+//  ePawss
+//
+//  Created by Laura Leal on 17/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ePawssApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ePawssTests/ePawssTests.swift
@@ -0,0 +1,16 @@
+//
+//  ePawssTests.swift
+//  ePawssTests
+//
+//  Created by Laura Leal on 17/11/25.
+//
+
+import Testing
+
+struct ePawssTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
